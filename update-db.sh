5fe14750f08ae1e05a4be5cd3a5a9652ba7a7d4f
greenlucid <greenfirechaos@gmail.com>
2022-01-03 19:07:54 +0100

    fix: use npx pm2 so that cron knows

--- update-db.sh
+++ update-db.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # move to vagarish dir
 cd `dirname "$0"`
-pm2 stop "npm run start"
+npx pm2 stop "npm run start"
 npm run init
-pm2 start "npm run start"
+npx pm2 start "npm run start"
